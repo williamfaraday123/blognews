1132054a17a26186449b52920dd11e46765d3de5
williamfaraday123 <williamfaraday123@gmail.com>
2024-12-06 14:02:48 +0800

    fixed error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import Footer from "@/components/footer/Footer";
-import Navbar from "@/components/navbar/navbar";
+import Navbar from "@/components/navbar/Navbar";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
 import localFont from "next/font/local";
