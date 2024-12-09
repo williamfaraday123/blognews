159cd752079c102fd049ffd8e9c3781cfe5dafbd
williamfaraday123 <williamfaraday123@gmail.com>
2024-12-10 01:08:50 +0800

    added console log to check dbPath

--- src/app/api/db.js
+++ src/app/api/db.js
@@ -1,9 +1,12 @@
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
+const dbPath = process.env.SQLITE_DB_PATH || './src/database/database.sqlite';
+
 const connectToDatabase = async () => {
+  console.log(dbPath); //check dbPath for error
   return open({
-    filename: process.env.SQLITE_DB_PATH || './src/database/database.sqlite',
+    filename: dbPath,
     driver: sqlite3.Database,
   });
 };
