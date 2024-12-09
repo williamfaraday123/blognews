914efaecf6fa331f8c54aaa062ea847eeb538abe
williamfaraday123 <williamfaraday123@gmail.com>
2024-12-10 01:37:47 +0800

    change to temp storage for sqlite on vercel

--- src/app/api/db.js
+++ src/app/api/db.js
@@ -1,10 +1,19 @@
+import path from 'path';
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
-const dbPath = process.env.SQLITE_DB_PATH || './src/database/database.sqlite';
+const getDatabasePath = () => {
+  // if in serverless environment like Vercel, use /tmp directory
+  if (process.env.VERCEL) {
+    return path.join('/tmp', 'database.sqlite'); // sqlite file in Vercel's temp storage
+  } else {
+    return process.env.SQLITE_DB_PATH || path.join(process.cwd(), 'src', 'database', 'database.sqlite'); //local development path
+  }
+}
 
 const connectToDatabase = async () => {
-  console.log(dbPath); //check dbPath for error
+  const dbPath = getDatabasePath();
+  console.log('Database path:', dbPath); //check dbPath for error
   return open({
     filename: dbPath,
     driver: sqlite3.Database,
